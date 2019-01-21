2588e3a9364f34b7b938de9a0554db940c1e39f8
Rosalía Serrano Herrera <46898135+RossySH@users.noreply.github.com>
2019-01-21 09:40:21 -0600

    Rosalía Serrano Herrera, Grupo 02.

---
+++ VelocidadPromedio.py
@@ -0,0 +1,9 @@
+# Rosalía Serrano Herrera
+# Calcula la velocidad promedio de un viaje
+
+tiempo = int(input("Teclea el tiempo de viaje en horas enteras: "))
+distancia = int(input("Teclea la distacia del viaje en kilometros enteros: "))
+
+velocidad = distancia / tiempo
+
+print("La velocidad promedio es: ", velocidad, "km/h")
